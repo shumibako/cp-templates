bcf2b9c1017cdbab7746d6aa2ae17e3a991843f2
shumibako <bo13shumi@gmail.com>
2024-10-30 01:08:26 +0900

    Refactor isInsideTriangle function to handle cases where the point lies on the triangle edges

--- isInsideTriangle.cpp
+++ isInsideTriangle.cpp
@@ -5,5 +5,7 @@ bool isInsideTriangle(pair<long long,long long> p, pair<long long,long long> a, 
     long long ABP = (a.first * (b.second - p.second) + b.first * (p.second - a.second) + p.first * (a.second - b.second));
     long long BCP = (b.first * (c.second - p.second) + c.first * (p.second - b.second) + p.first * (b.second - c.second));
     long long CAP = (c.first * (a.second - p.second) + a.first * (p.second - c.second) + p.first * (c.second - a.second));
-    return (ABP >= 0 && BCP >= 0 && CAP >= 0);
+    if(ABP >= 0 && BCP >= 0 && CAP >= 0)return true;
+    if(ABP <= 0 && BCP <= 0 && CAP <= 0)return true;
+    return false;
 }
